4578436fded5eb3a0f9b7628c6672b824f027193
zydroot <395724328@qq.com>
2019-04-08 22:13:23 +0800

    rate limit

--- app/routes/api.php
+++ app/routes/api.php
@@ -21,12 +21,19 @@ $api->version('v1', [
     $api->get('version', function() {
         return response('this is version v1');
     });
-    // 短信验证码
-    $api->post('verificationCodes', "VerificationCodesController@store")
-        ->name('api.verificationCodes.store');
-    // 用户注册
-    $api->post('user', "UsersController@store")
-        ->name('api.users.store');
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit'  => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires')
+    ],function($api){
+        // 短信验证码
+        $api->post('verificationCodes', "VerificationCodesController@store")
+            ->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('user', "UsersController@store")
+            ->name('api.users.store');
+    });
+
 
 });
 
